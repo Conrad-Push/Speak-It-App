c957ad36edb4301e30be80d6f7415d3f04a184ec
Conrad-Push <konrad.pusz7@gmail.com>
2023-09-06 00:18:08 +0200

    Constants files (texts, colors) added

---
+++ lib/constants/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+
+class AppColors {
+  static const primaryColor = Color(0xFF0077FF);
+  static const secondaryColor = Color(0xFFFFA500);
+}
---
+++ lib/constants/texts.dart
@@ -0,0 +1,9 @@
+class EnglishTexts {
+  static const welcomeMessage = "Welcome to Speak It App";
+}
+
+
+class PolishTexts {
+  static const welcomeMessage = "Witaj w Speak It App";
+}
+
